b33613d9218d7429b6e848bddfb84b9c8e6569b2
unknown <khhan1990@hanmail.net>
2022-08-03 21:49:50 +0900

    비밀 정보 보호까지 완성

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+};
+
+// 나중에 배포할 떄 HEROKU에 MONGO_URI를 넣어주면 된다.
--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://khhan1990:hankyu5134@mongodbpr.afibsec.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log("MongoDB is connected");
   })
@@ -16,6 +21,19 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.status(400).send({ err: err.message });
+    return res.status(200).send({
+      success: userInfo,
+    });
+  });
 });
 
 app.listen(port, () => {
