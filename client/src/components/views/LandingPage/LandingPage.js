449abfff709c9f780fc587a2fe869bf33ce36e02
unknown <khhan1990@hanmail.net>
2022-08-05 22:05:26 +0900

    axios 연결까지 완성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function LandingPage() {
-  return (
-    <div>LandingPage</div>
-  )
+  useEffect(() => {
+    axios.get("/api/hello").then((res) => {console.log(res)});
+  }, []);
+
+  return <div>LandingPage</div>;
 }
 
-export default LandingPage
+export default LandingPage;
